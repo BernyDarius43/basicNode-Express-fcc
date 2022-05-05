f7558038d0d287439b92542ef95f04f6eb181839
Berny <bernydarius01@gmail.com>
2022-05-05 14:56:50 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 let absolutePath =  __dirname + '/views/index.html'
 let absolutePathCss = __dirname + '/public'
-app.use(express.static(absolutePathCss))
+app.use('/public', express.static(absolutePathCss))
 
 app.get('/', (req, res) => {
     //res.send('Hello Express')

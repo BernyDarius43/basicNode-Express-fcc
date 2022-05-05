6782b674db6d0ac9ab43361e4e399f08fb37902d
Berny <bernydarius01@gmail.com>
2022-05-05 14:28:19 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-let absolutePath = './views/index.html'
+var absolutePath =  __dirname + 'views/index.html'
 
 app.get('/', (req, res) => {
     //res.send('Hello Express')

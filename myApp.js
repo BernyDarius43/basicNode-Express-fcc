851b1609c8dfe8e92ffbc4e8f0a813c41c44c390
Berny <bernydarius01@gmail.com>
2022-05-13 11:49:12 -0400

    .env challenge done

--- myApp.js
+++ myApp.js
@@ -1,15 +1,18 @@
 let express = require('express');
+require('dotenv').config()
 let app = express();
-let absolutePath =  __dirname + '/views/index.html'
+let absolutePath = __dirname + '/views/index.html'
 let absolutePathCss = __dirname + '/public'
 app.use('/public', express.static(absolutePathCss))
 
 app.get('/', (req, res) => {
-    //res.send('Hello Express')
-    res.sendFile(absolutePath)
-    // console.log(`Hello World! WOOOO!`);
-  });
-app.get('/json', (req, res) => {
-  res.json({message: "Hello json"})
+  //res.send('Hello Express')
+  res.sendFile(absolutePath)
+  // console.log(`Hello World! WOOOO!`);
 });
- module.exports = app;
+app.get('/json', (req, res) => {
+  if (process.env.MESSAGE_STYLE != "uppercase") {
+    res.json({message: "failed"})
+  } else res.json({ message: "HELLO JSON" })
+});
+module.exports = app;
